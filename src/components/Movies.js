76f54965d0600fd4ac6d926c5aaaa5820b1a6dfa
cailinkless <cailin.nysff@gmail.com>
2021-01-21 17:17:14 -0500

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,9 +2,19 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+  // debugger
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+        {movies.map((movie, index) => (
+          <div key={index}>
+            <h2>{movie.title}</h2>
+            <p>{movie.time}</p>
+            <ul>
+              {movie.genres.map(genre => <li>{genre}</li>)}
+            </ul>
+          </div>
+        ))}
     </div>
   );
 };
