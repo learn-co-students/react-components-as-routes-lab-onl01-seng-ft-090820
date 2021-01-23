aa91121dbaf55edaf244010125f51e8a6c4dd971
doosemavis <moosedavis2011@gmail.com>
2021-01-23 13:48:15 -0600

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,9 +2,22 @@ import React from 'react';
 import { actors } from '../data';
 
 const Actors = () => {
+  const actorsList = actors.map(actor => {
+    const moviesLis = actor.movies.map(movie => <li>{movie}</li>)
+    return (
+      <div>
+        <h2>{actor.name}</h2>
+        <ul>
+          {moviesLis}
+        </ul>
+      </div>
+    )
+  })
+
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actorsList}
     </div>
   );
 };
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,11 +2,22 @@ import React from 'react';
 import { directors } from '../data';
 
 const Directors = () => {
+  const directorsList = directors.map(director => {
+    const moviesLis = director.movies.map(movie => <li>{movie}</li>)
+    return (
+      <div> 
+        <h2>{director.name}</h2>
+        <ul>
+          {moviesLis}
+        </ul>
+      </div> )}
+    )
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {directorsList}
     </div>
   );
 }
 
-export default Directors
+export default Directors;
